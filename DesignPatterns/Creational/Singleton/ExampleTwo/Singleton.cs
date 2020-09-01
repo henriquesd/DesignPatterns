be5acf1e49b62cf1f537f2d87a774c3ddbb4e4f7
henriquesd <hsd.foz@gmail.com>
2020-09-01 19:28:11 +0200

    Implemented Singleton

---
+++ DesignPatterns/Creational/Singleton/ExampleTwo/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesignPatterns.Creational.Singleton.ExampleTwo
+{
+    public class Singleton
+    {
+        public static void Execute()
+        {
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+            var b4 = LoadBalancer.GetLoadBalancer();
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            var balancer = LoadBalancer.GetLoadBalancer();
+
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                Console.WriteLine("Dispatch request to: " + serverName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace DesignPatterns
+﻿using System;
+
+namespace DesignPatterns
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            
+            Console.WriteLine("Choose the operation:");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Creational Patterns");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("1 - Singleton Example One");
+            Console.WriteLine("2 - Singleton Example Two");
+            Console.WriteLine("------------------------");
+
+            var option = Console.ReadKey();
+
+            Console.WriteLine("");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("");
+
+            switch (option.KeyChar)
+            {
+                case '1':
+                    Creational.Singleton.ExampleOne.Singleton.Execute();
+                    break;
+                case '2':
+                    Creational.Singleton.ExampleTwo.Singleton.Execute();
+                    break;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Main();
         }
     }
 }
